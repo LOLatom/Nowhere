93e6d8a73f415e6b39f7ca500fe9e14c86fca76a
Xyndra <71836523+Xyndra@users.noreply.github.com>
2024-08-18 02:15:47 +0200

    Make the white noise variable

--- src/main/java/com/thefreak/nowhere/client/renderers/TheLocustRenderer.java
+++ src/main/java/com/thefreak/nowhere/client/renderers/TheLocustRenderer.java
@@ -7,7 +7,6 @@ import com.thefreak.nowhere.common.entities.TheLocust;
 import com.thefreak.nowhere.common.initiation.NowhereRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -21,13 +20,9 @@ public class TheLocustRenderer extends GeoEntityRenderer<TheLocust> {
     public void render(TheLocust entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
         poseStack.scale(0.6F,0.6F,0.6F);
         ResourceLocation texture = new ResourceLocation(Nowhere.MODID,"textures/entity/the_locust.png");
-        super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+        this.animatable = entity;
 
-        reRender(model.getBakedModel(new ResourceLocation(Nowhere.MODID, "geo/the_locust.geo.json"))
-                , poseStack, bufferSource, animatable, NowhereRenderTypes.staticNoCull(texture),
-                bufferSource.getBuffer(NowhereRenderTypes.staticNoCull(texture)), partialTick
-                , 15728640, OverlayTexture.NO_OVERLAY,
-                1, 1, 1, 1);
-
+        defaultRender(poseStack, entity, bufferSource, NowhereRenderTypes.staticCull(texture), null,
+                entityYaw, partialTick, packedLight);
     }
 }
--- src/main/resources/assets/nowhere/pinwheel/shaders/program/static_shader.fsh
+++ src/main/resources/assets/nowhere/pinwheel/shaders/program/static_shader.fsh
@@ -1,11 +1,40 @@
-const float e = 2.71828182846f;
-in vec2 texCoord0;
+#include veil:fog
+
+uniform sampler2D Sampler0;
+
+uniform vec4 ColorModulator;
+uniform float FogStart;
+uniform float FogEnd;
+uniform vec4 FogColor;
 uniform float GameTime;
+uniform float staticPercentage;
+
+in float vertexDistance;
+in vec4 vertexColor;
+in vec4 lightMapColor;
+in vec4 overlayColor;
+in vec2 texCoord0;
+
 out vec4 fragColor;
+
+const float e = 2.71828182846f;
+
 void main() {
+    vec4 color = texture(Sampler0, texCoord0);
+    if (color.a < 0.1) {
+        discard;
+    }
+    color *= vertexColor * ColorModulator;
+    color.rgb = mix(overlayColor.rgb, color.rgb, overlayColor.a);
+    color *= lightMapColor;
     // TODO: Make 250 be the amount of pixels visible on the screen
-	vec2 texCoord = texCoord0.xy * 250;
+    vec2 texCoord = texCoord0.xy * 250;
 	float G = e + (GameTime * 10f);
 	vec2 r = (G * sin(G * texCoord.xy));
-	fragColor = vec4(fract(r.x * r.y * (1.0f + texCoord.x)));
+	vec3 fullColor = vec3(fract(r.x * r.y * (1.0f + texCoord.x)));
+	vec4 oldColorPart = color * (1.0 - staticPercentage);
+	vec4 noisePart = vec4(fullColor, 1.0) * staticPercentage;
+	color = oldColorPart + noisePart;
+    color *= vec4(fullColor.xyz, 1.0);
+    fragColor = linear_fog(color, vertexDistance, FogStart, FogEnd, FogColor);
 }
